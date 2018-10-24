be0c35d25b76d52fce25fda668b06c2699d1384c
Yana Shulga <yanashulga@iMac-Yana.local>
2018-10-24 22:34:52 +0200

    axios  added with defaults settings

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
-import axios from "axios";
+// import axios from "axios";
+import axios from "../../axios";
 import Post from "../../components/Post/Post";
 import FullPost from "../../components/FullPost/FullPost";
 import NewPost from "../../components/NewPost/NewPost";
@@ -19,30 +20,39 @@ class Blog extends Component {
       //func will be execited only after promise
       .then(response => {
         const posts = response.data.slice(0, 4);
-        const updatePosts = posts.map(post => {
+        const updatedPosts = posts.map(post => {
           return {
             ...post,
-            author: "Yana"
+            author: "Yana",
+            error: false
           };
         });
-        this.setState({ posts: updatePosts });
-        console.log(response);
+        this.setState({ posts: updatedPosts });
+        // console.log(response);
+      })
+      .catch(error => {
+        //   console.log(error)
+        this.setState({ error: true });
       });
   }
   postSelectedHandler = id => {
     this.setState({ selectedPostId: id });
   };
   render() {
-    const posts = this.state.posts.map(post => {
-      return (
-        <Post
-          key={post.id}
-          title={post.title}
-          author={post.author}
-          clicked={() => this.postSelectedHandler(post.id)}
-        />
-      );
-    });
+    let posts = <p>Something went wrong!</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return (
+          <Post
+            key={post.id}
+            title={post.title}
+            author={post.author}
+            clicked={() => this.postSelectedHandler(post.id)}
+          />
+        );
+      });
+    }
+
     return (
       <div>
         <section className="Posts">{posts}</section>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import axios from "axios";
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+//if you want to use the same route path or look at axios.js
+axios.defaults.baseURL = "https://jsonplaceholder.typicode.com";
+axios.defaults.headers.common["Authorization"] = "AUTH TOKEN";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
+axios.interceptors.request.use(
+  request => {
+    console.log(request);
+    //or adit the request config
+    return request;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+axios.interceptors.response.use(
+  response => {
+    console.log(response);
+    //or adit the request config
+    return response;
+  },
+  error => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
